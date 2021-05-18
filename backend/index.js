996895b52e798ff4ddee4fb81ea0d20f1ef14da2
Shashi Kumar Sah <shashichander009@gmail.com>
2021-05-18 22:06:49 +0530

    changed message

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Shashi!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(` listening at http://localhost:${port}`);
 });
